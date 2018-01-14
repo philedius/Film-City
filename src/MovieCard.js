28561f977d6bba9b13be55d473742364f2d5161a
philedius <fannarr@gmail.com>
2018-01-14 22:41:21 +0100

    Loading actual data now from TMDB. Interface tweaks

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Plus, Minus, XSymbol, View, Star } from './Icons'
+import { Plus, Minus, XSymbol, View, Star, Info } from './Icons'
 
-export default ({ title, image }) => 
-<div className="movie-card" style={{backgroundImage: `url(${image})`}}>
+export default (props) => 
+<div className="movie-card" id={props.movie.id} style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w342${props.movie.poster_path})` }}>
+    <div className="info">
+    <h1>{props.movie.title}</h1>
+    <p>{props.movie.overview}</p>
+    </div>
     <div className="sidebar">
       <div className="sidebar-icons">
         <Plus />
         <View />
         <XSymbol />
-        
+        <Info />
       </div>
     </div>
 </div>;
