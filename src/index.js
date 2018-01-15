9e4fd6e0b47187a976b520fe08776244813c3202
philedius <fannarr@gmail.com>
2018-01-15 10:40:18 +0100

    Quick pagination implementation. Making grid more responsive.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import MovieCard from './MovieCard';
 import './style.css';
 const mdb = require('moviedb')('3390228324e8a091fabebee2d57020a4');
 var results = [];
-mdb.searchMovie({query: 'bad'}, (err, res) => {
-  console.log(res);
-  results = res.results;
-  render(<App />, document.getElementById('root'));
-});
+// mdb.discoverMovie({ page: 1 }, (err, res) => {
+//   console.log(res);
+//   results = res.results;
+//   render(<App />, document.getElementById('root'));
+// });
+
+function getPage(page = 1) {
+  mdb.discoverMovie({ page: page }, (err, res) => {
+    console.log(res);
+    results = res.results;
+    render(<App />, document.getElementById('root'));
+    window.scrollTo(0, 0);
+  });
+}
 
 function MovieGrid(props) {
   const movies = props.movies;
@@ -31,5 +40,17 @@ const App = () => (
   <div id="container">
   <Filters />
   <MovieGrid movies={results} />
+  <div id="pagination">
+    <span onClick={(e) => getPage(1)}>1</span>
+    <span onClick={(e) => getPage(2)}>2</span>
+    <span onClick={(e) => getPage(3)}>3</span>
+    <span onClick={(e) => getPage(4)}>4</span>
+    <span onClick={(e) => getPage(5)}>5</span>
+    <span onClick={(e) => getPage(6)}>6</span>
+    <span onClick={(e) => getPage(7)}>7</span>
+    <span onClick={(e) => getPage(8)}>8</span>
+  </div>
   </div>
 );
+
+getPage(2);
