eee42b0530a8fb5bce25acdd6b4ebc691fd6847c
philedius <fannarr@gmail.com>
2018-04-29 22:56:47 +0200

    Started using axios for GET requests. Started fetching data from OMDB. Fixed problem with Vibrant. Style changes.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import InfiniteScroll from 'react-infinite-scroller';
 import MovieCard from './MovieCard';
 import './style.css';
 const Vibrant = require('node-vibrant');
 const mdb = require('moviedb')('3390228324e8a091fabebee2d57020a4');
+const tmdb = '3390228324e8a091fabebee2d57020a4';
+const omdb = 'bdc8301e';
 let results = [];
 let totalItems = [];
-const discoverMovie = mdb.discoverMovie;
 
-mdb.configuration((error, response) => {
-  window.config = response;
-  console.log(window.config);
-})
+// mdb.configuration((error, response) => {
+//   window.config = response;
+//   console.log(window.config);
+// });
+
+mdb.genreMovieList((error, response) => {
+  if (!error) {
+    console.log('Genres', response.genres);
+  } else {
+    console.error(error);
+  }
+});
 
 // TODO: keep items between going from MoviePage to Home
 class MovieGrid extends React.Component {
@@ -25,13 +35,13 @@ class MovieGrid extends React.Component {
   }
 
   componentDidMount() {
-    
     this.allItems = [];
     this.currentItems = [];
+    
   }
   
   getPage() {
-    let hehe = mdb.discoverMovie({ page: this.state.currentPage, include_adult: false }, (error, response) => {
+    mdb.discoverMovie({ with_genres: '80', page: this.state.currentPage, include_adult: false }, (error, response) => {
       if (!error) {
         this.currentItems = response.results;
         this.incrementPage();
@@ -91,45 +101,72 @@ class App extends React.Component {
 class MoviePage extends React.Component {
   constructor(props) {
     super(props);
-    this.color = 'rgba(0, 0, 0, 0.5)'
     this.state = {
       title: '',
       score: '',
       overview: '',
+      ratings: [],
     };
   }
 
   componentDidMount() {
-    mdb.movieInfo({ id: this.props.match.params.movieId }, (error, response) => {
-      console.log(response);
-      
-      Vibrant.from(window.config.images.base_url + window.config.images.backdrop_sizes[3] + response.backdrop_path).getPalette((err, palette) => {
-        console.log(palette);
-        this.setState({
-          backdropColor: `rgba(${Math.floor(palette.Vibrant.r)}, ${Math.floor(palette.Vibrant.g)}, ${Math.floor(palette.Vibrant.b)}, 0.75)`
-        })
-      });
 
-      this.setState({
-        title: response.original_title,
-        score: response.vote_average,
-        overview: response.overview,
-        backdrop: window.config.images.base_url + window.config.images.backdrop_sizes[3] + response.backdrop_path,
-        poster: window.config.images.base_url + window.config.images.poster_sizes[3] + response.poster_path,
-        releaseYear: response.release_date.substring(0, 4)
+    axios.get(`https://api.themoviedb.org/3/movie/${this.props.match.params.movieId}?api_key=${tmdb}&append_to_response=credits`)
+      .then((response) => {
+        response = response.data;
+        axios.get(`http://www.omdbapi.com/?i=${response.imdb_id}&apikey=${omdb}`)
+        .then((res) => {
+          console.log(res.data);
+          this.setState({
+            ratings: res.data.Ratings
+          });
+          for (let i = 0; i < res.data.Ratings.length; i++) {
+            let rating = res.data.Ratings[i];
+            console.log(rating.Source, rating.Value);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+        console.log(response);
+        Vibrant.from('http://image.tmdb.org/t/p/w92' + response.poster_path).getPalette((err, palette) => {
+          this.setState({
+            backdropColor: getGradientFromPalette(palette)
+          });
+  
+          let container = document.getElementsByClassName('movie-container')[0];
+          container.style.opacity = 1;
+          container.style.transform = 'scale(1)';
+        });
+  
+        this.setState({
+          title: response.title,
+          score: response.vote_average,
+          overview: response.overview,
+          backdrop: 'http://image.tmdb.org/t/p/w1280' + response.backdrop_path,
+          poster: 'http://image.tmdb.org/t/p/w342' + response.poster_path,
+          releaseYear: response.release_date.substring(0, 4),
+
+        });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    });
   }
   
   render() {
+        const Ratings = this.state.ratings.map((rating) => 
+        <span className="rating" key={rating.Source}>{rating.Value}</span>
+      );
         return (
           <div className="movie-container">
             <div className="movie-backdrop" style={{backgroundImage: `url(${this.state.backdrop})`}}>
-                <div className="movie-backdrop-color-overlay" style={{backgroundColor: this.state.backdropColor}}>
+                <div className="movie-backdrop-color-overlay" style={{background: this.state.backdropColor}}>
                 <div className="movie-info-container">
                   <div className="movie-poster" style={{backgroundImage: `url(${this.state.poster})`}}></div>
                   <div className="movie-text-info">
                     <h1>{this.state.title}<span className="release-year">({this.state.releaseYear})</span></h1>
+                    <div className="movie-ratings">{Ratings}</div>
                     <div className="movie-overview"><p>{this.state.overview}</p></div>
                   </div>
                 </div>
@@ -163,9 +200,58 @@ const Filters = () => (
   <div id="filters">
     <div id="filter-components">
       <h3>Released in</h3>
+      <input className="slider" type="range" />
       <h3>Minimum score</h3>
+      <input className="slider" type="range" />
+      <input className="slider" type="range" />
+      <input className="slider" type="range" />
       <h3>Genres</h3>
     </div>
   </div>
 )
+
+const getGradientFromPalette = (palette) => {
+  let primaryColor;
+  let secondaryColor;
+  for (const color in palette) {
+    if (palette[color] === null) continue;
+    if (primaryColor === undefined) {
+      primaryColor = color;
+    };
+    if (palette[color]._population > palette[primaryColor]._population) {
+      primaryColor = color;
+    }
+  }
+
+  for (const color in palette) {
+    if (palette[color] === null) continue;
+    if (color !== primaryColor) {
+      if (secondaryColor === undefined) {
+        secondaryColor = color;
+      };
+      if (secondaryColor !== undefined) {
+        if (palette[color]._population > palette[secondaryColor]._population) {
+          secondaryColor = color;
+        }
+      }
+    }
+  }
+
+  primaryColor = {
+    h: Math.floor(palette[primaryColor]._hsl[0] * 360),
+    s: Math.floor(palette[primaryColor]._hsl[1] * 150).toString() + '%',
+    l: Math.floor(palette[primaryColor]._hsl[2] * 100).toString() + '%',
+  }
+
+  secondaryColor = {
+    h: Math.floor(palette[secondaryColor]._hsl[0] * 360),
+    s: Math.floor(palette[secondaryColor]._hsl[1] * 150).toString() + '%',
+    l: Math.floor(palette[secondaryColor]._hsl[2] * 100).toString() + '%',
+  }
+  primaryColor = `hsla(${primaryColor.h}, ${primaryColor.s}, ${primaryColor.l}, .925)`;
+  secondaryColor = `hsla(${secondaryColor.h}, ${secondaryColor.s}, ${secondaryColor.l}, .925)`;
+  return `linear-gradient(45deg, ${primaryColor}, ${secondaryColor})`;
+
+}
+
 render(<App />, document.getElementById('root'));
